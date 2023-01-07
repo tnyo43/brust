7cac4e739cc3f3a07b8550cfef243fe5954c833e
Tomoya Kashifuku <tnyo43@gmail.com>
2023-01-07 16:25:20 +0900

    add parsing selector

--- src/css.rs
+++ src/css.rs
@@ -1,4 +1,7 @@
-use crate::{parser::Parser, style::Declaration};
+use crate::{
+    parser::Parser,
+    style::{Declaration, Selector},
+};
 
 struct CSSParser {
     parser: Parser,
@@ -11,11 +14,44 @@ impl CSSParser {
         }
     }
 
-    fn parse_name(&mut self) -> String {
+    fn is_valid_identifier_initial_char(&self) -> bool {
+        match self.parser.next_char() {
+            'a'..='z' | 'A'..='Z' => true,
+            _ => false,
+        }
+    }
+
+    fn parse_identifier(&mut self) -> String {
         self.parser.consume_while(|c| match c {
-            'a'..='z' | 'A'..='Z' | '0'..='9' | '-' => true,
+            'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' => true,
             _ => false,
         })
+    }
+
+    fn parse_selector(&mut self) -> Selector {
+        let mut selector = Selector::new(None, None, Vec::new());
+
+        while !self.parser.eof() {
+            self.parser.consume_whitespace();
+            match self.parser.next_char() {
+                '#' => {
+                    self.parser.consume_char();
+                    selector.id = Some(self.parse_identifier());
+                }
+                '.' => {
+                    self.parser.consume_char();
+                    selector.class.push(self.parse_identifier());
+                }
+                _ if self.is_valid_identifier_initial_char() => {
+                    selector.tag = Some(self.parse_identifier());
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+
+        selector
     }
 
     fn parse_declarations(&mut self) -> Vec<Declaration> {
@@ -31,7 +67,7 @@ impl CSSParser {
                 break;
             }
 
-            let name = self.parse_name();
+            let name = self.parse_identifier();
 
             self.parser.consume_whitespace();
             assert!(self.parser.consume_char() == ':');
@@ -58,6 +94,26 @@ mod tests {
     use super::*;
 
     speculate! {
+        describe "'parse_selector' parse selector" {
+            #[ignore]
+            #[rstest(input, expected,
+                case(".hoge__fizz-bar", Selector::new(None, None, Vec::from(["hoge__fizz-bar".to_string()]))),
+                case(
+                    "div.a.b.c.d",
+                    Selector::new(Some("div".to_string()), None,  Vec::from(["a".to_string(), "b".to_string(), "c".to_string(), "d".to_string() ])),
+                ),
+                case(
+                    "button#submit_name.main",
+                    Selector::new(Some("button".to_string()), Some("submit_name".to_string()), Vec::from(["main".to_string() ]))
+                ),
+            )]
+            fn test_parse_tag_id_class(input: &str, expected: Selector) {
+                let mut css_parser = CSSParser::new(input.to_string());
+
+                assert_eq!(css_parser.parse_selector(), expected);
+            }
+        }
+
         describe "'parse_declarations' parse declaration block" {
             #[rstest]
             fn test_empty_block() {
--- src/style.rs
+++ src/style.rs
@@ -1,7 +1,24 @@
 #[derive(Debug, PartialEq)]
+pub struct Selector {
+    pub tag: Option<String>,
+    pub id: Option<String>,
+    pub class: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Declaration {
     name: String,
     value: String,
+}
+
+impl Selector {
+    pub fn new(tag: Option<String>, id: Option<String>, class: Vec<String>) -> Self {
+        Selector {
+            tag: tag,
+            id: id,
+            class: class,
+        }
+    }
 }
 
 impl Declaration {
